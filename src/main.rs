76e86024c41810dd16e5d484708961daf821eb3d
Thomas Cowart <thomas.cowart@duke.edu>
2022-07-05 09:56:11 -0400

    Improve top-level error handling
    Actually handle errors when building data or getting the DB connection.

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,25 @@ mod build_data;
 mod data_structures;
 mod options;
 
-use postgres::{Client, Error, NoTls};
+use postgres::{Client, NoTls};
 
 use crate::build_data::build_data;
 use crate::options::Options;
 
 type DbID = i64;
 
-fn main() -> Result<(), Error> {
+fn main() {
     let options = Options::get();
 
-    let mut client = Client::connect(&options.connection_string, NoTls)?;
+    let mut client = match Client::connect(&options.connection_string, NoTls) {
+        Ok(client) => client,
+        Err(e) => {
+            panic!("{}", e)
+        }
+    };
 
-    let data = build_data(&options, &mut client)?;
-
-    data.serialize(&options.output_location);
-
-    Ok(())
+    match build_data(&options, &mut client) {
+        Ok(data) => data.serialize(&options.output_location),
+        Err(e) => eprintln!("{}", e),
+    };
 }
