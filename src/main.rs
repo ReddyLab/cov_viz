45a5db1b165ded8423638c204a2b095439cb2c98
Thomas Cowart <thomas.cowart@duke.edu>
2022-07-12 16:05:31 -0400

    Remove superfluous deserialization code
    This code doesn't really do anything in the context of this program.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ mod build_data;
 mod data_structures;
 mod options;
 
-use std::time::Instant;
-
-use data_structures::CoverageData;
 use postgres::{Client, NoTls};
 
 use crate::build_data::build_data;
@@ -24,8 +21,4 @@ fn main() {
         Ok(data) => data.serialize(&options.output_location),
         Err(e) => eprintln!("{}", e),
     };
-
-    let now = Instant::now();
-    let _ = CoverageData::deserialize(&options.output_location);
-    println!("Time to decode: {}ms", now.elapsed().as_millis());
 }
